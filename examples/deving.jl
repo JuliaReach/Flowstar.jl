4512ca5d7cbce0f53a8c9e6e89cae4144e2e3c2e
Adam R. Gerlach <adam.gerlach.1@afresearchlab.com>
2022-06-17 17:08:36 +0000

    initial parsing

---
+++ examples/deving.jl
@@ -0,0 +1,103 @@
+using Pkg; cd(@__DIR__); Pkg.activate("..")
+
+using Flowstar, TaylorModels, ReadableRegex
+
+
+
+# Read *.flow file
+S = flowstar(joinpath(abspath("../test/models"),"lv.model"); outdir = pwd())
+Sheader, Slocal, Sbody = split(S, "{", limit = 3)
+
+fph = parse(FlowpipeHeader, Sheader)
+
+local_vars = ["local_t"; split(Flowstar.match_between(Slocal, "tm var "), ",")]
+
+# varmap = lvars .=> vars
+
+ex =Flowstar._parse(Sbody, order(fph), nstates(fph)+1, local_vars)
+Flowstar.x
+
+P = Flowstar._parse2(Sbody, order(fph), nstates(fph)+1,states(fph), local_vars, 3)
+
+
+# Parse Body
+body = split(Sbody, "{")
+
+res = map(body) do b
+    s= split(b, "\n\n\n")[1]
+    s = replace.(s, "}"=>"")
+    s = strip.(s)
+    s = replace.(s, "\n" => ";")
+    s = replace.(s, "[" => "(")
+    s = replace.(s, "]" => ")")
+    s = replace.(s, "," => "..")
+    for (idx,lv) in enumerate(local_vars)
+        s = replace(s, "$lv" =>"ξ[$idx]")
+    end
+    "function _fp(ξ); $(s); return [$(join(states(fph), ","))]; end"
+end
+
+# body = replace.(body, "}"=>"")
+# body = strip.(body)
+# body = replace.(body, "\n" => ";")
+# body = replace.(body, "[" => "(")
+# body = replace.(body, "]" => ")")
+# body = replace.(body, "," => "..")
+
+
+
+
+# body = replace.(body, " in" =>"δ =")
+# body = map(body[1:1]) do b
+#     for (idx,lv) in enumerate(local_vars)
+#         b = replace(b, "$lv" =>"X[$idx]")
+#         b = replace(b, "X[$idx]δ" => "$")
+#     end
+#     b
+# end
+
+# body = replace.(body, "local_t" => "X[1]")
+
+
+# pushfirst!(body[1], "function hello($path"
+
+
+
+ξ = set_variables(join(["t"; states(fph)]," "), order=order(fph))
+
+ex = Meta.parse(res[1])
+
+eval(ex)
+
+_fp(ξ)
+
+# MAKE FLOWSTARSOLUTION STRUCT -> DISPATCH FOR TM CONSTRUCTOR????
+
+using Symbolics
+
+@variables r t
+
+@variables local_var_1 local_var_2
+local_t = TaylorModel1(6,0.0..3.0)
+
+local_t*local_var_1
+
+import Base.*
+function *(a::Taylor1{S},b::Num) where S<:Union{Real, Complex}
+a*b
+end
+
+
+
+
+f = 2*r + r*t + r^2*t + t^2
+expand(f)
+simplify(f)
+
+[2:end-3]
+
+re = r"\{([^{}]+)\}"
+
+m=eachmatch(re, S) |> collect;
+
+m|> length
--- src/Flowstar.jl
+++ src/Flowstar.jl
@@ -1,7 +1,11 @@
 module Flowstar
 
-using Flowstar_jll
+using Flowstar_jll, TaylorModels, RuntimeGeneratedFunctions
+RuntimeGeneratedFunctions.init(@__MODULE__)
+
 import Flowstar_jll: flowstar
+import Base: parse
+
 
 function flowstar(model; outdir = mktempdir())
     cmd = Cmd(`$(flowstar())`; dir = outdir)
@@ -11,6 +15,89 @@ function flowstar(model; outdir = mktempdir())
     String(read(flow))
 end
 
-export flowstar
+function match_between(s::AbstractString, left, right = "\\n\\r")
+    re= Regex("(?<=\\Q$left\\E)(.*?)(?=[$right])")
+    m = match(re, s)
+
+    @assert !isnothing(m) "No regex matches found between $left and $right"
+    m.match
+end
+
+struct FlowpipeHeader{𝕊}
+    states::Vector{𝕊}
+    order::Int
+    cutoff::Float64
+    output::𝕊
+    # locals::Vector{𝕊}
+end
+
+function parse(::Type{FlowpipeHeader}, str)
+    states = split( match_between(str, "state var "), ",")
+    order = parse(Int, match_between(str, "order "))
+    cutoff = parse(Float64, match_between(str, "cutoff "))
+    output = match_between(str, "output ")
+    # locals = split(match_between(Slocal, "tm var "), ",")
+    FlowpipeHeader(states, order, cutoff, output)
+end
+
+states(fp::FlowpipeHeader) = fp.states
+nstates(fp::FlowpipeHeader) = length(states(fp))
+order(fp::FlowpipeHeader) = fp.order
+cutoff(fp::FlowpipeHeader) = fp.cutoff
+
+function _parse(str, order, numvars, lvars; names = "ξ")
+    body = split(str, "{")
+    body = replace.(body, "}"=>"")
+    body = strip.(body)
+    body = replace.(body, "\n" => ";")
+    body = replace.(body, "[" => "(")
+    body = replace.(body, "]" => ")")
+    body = replace.(body, "," => "..")
+    body = replace.(body, " in" =>"δ =")
+
+
+    # local_t, local_var_1, local_var_2 = set_variables(names; order, numvars)
+    ex = Meta.parse(body[1])
+
+    _build_and_inject_function(@__MODULE__, ex)
+    # eval(ex)
+
+    # @show local_t
+end
+
+function _parse2(str, order, numvars, vars, lvars, idx; names = "ξ")
+    ξ = set_variables(names; order, numvars)
+
+    body = split(str, "{")
+
+    res = map(body) do b
+        s= split(b, "\n\n\n")[1]
+        s = replace.(s, "}"=>"")
+        s = strip.(s)
+        s = replace.(s, "\n" => ";")
+        s = replace.(s, "[" => "(")
+        s = replace.(s, "]" => ")")
+        s = replace.(s, "," => "..")
+        for (idx,lv) in enumerate(lvars)
+            s = replace(s, "$lv" =>"ξ[$idx]")
+        end
+        "function _fp(ξ); $(s); return [$(join(vars, ","))]; end"
+    end
+
+    map(res, 1:length(res)) do r, idx
+        @show idx
+        ex = Meta.parse(r)
+        _f = @RuntimeGeneratedFunction(ex)
+        _f(ξ)
+    end
+end
+
+
+function valid_file(s)
+    @assert occursin("continuous flowpipes", s) "Currently only continuous flowpipe solutions are supported"
+end
+
+
+export flowstar, FlowpipeHeader, states, nstates, order, cutoff
 end
 
